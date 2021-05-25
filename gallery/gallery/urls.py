668f636e013c746c3fac048a37079e753d25185a
fnyaoke <fnyaoke@gmail.com>
2021-05-25 15:41:04 +0300

    updated the gallery urls

--- gallery/gallery/urls.py
+++ gallery/gallery/urls.py
@@ -14,17 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
-from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^views/', views.index,name = 'index'),
-    path(r'^search/', views.search_image, name='search_image'),
-    path(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
-    path(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single,name = 'single')
+    path('', include('capture.urls'))
+
 ]
 
 if settings.DEBUG:
